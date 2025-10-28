b9304a07a18e886051fe586018b2a2c09c743386
bharathy2001 <bharathy2001gm@gmail.com>
2025-10-28 17:19:38 +0530

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,9 @@
+print("hello")
+print("BIODATA")
+print("My name: geethaharathy")
+print("my age: 24")
+print( "mother name: S.Meenakshi")
+print(" My address: 16 kaaja street near ny amirtham cinemas")
+print(" My Qualifiction: Msc Computer Sciences")
+      
+
